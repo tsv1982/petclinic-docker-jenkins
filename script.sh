ebc571cc255094d219cce889cbff518e5d1ce79f
Tarabanov Serhii <tsv198250@gmail.com>
2022-03-18 01:11:12 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-sudo apt-get update -y
+sudo apt update -y
 sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" -y
-sudo apt-get update -y
-sudo apt install docker-ce -y
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable" -y
+sudo apt update -y
+
 
 
 
