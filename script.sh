2d947b6a2de7c3d832615a5613cf5cd54d6258fe
Tarabanov Serhii <tsv198250@gmail.com>
2022-03-18 01:29:49 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,6 +4,8 @@ sudo apt install apt-transport-https ca-certificates curl software-properties-co
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable" -y
 sudo apt update -y
+sudo apt install docker-ce -y
+
 
 # sudo apt update -y
 # sudo apt install python3-pip -y
