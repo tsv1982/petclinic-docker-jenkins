8a8d8e4a18bc7d1e61588aceb55088f3c181be16
Tarabanov Serhii <tsv198250@gmail.com>
2022-03-18 01:28:28 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -5,6 +5,9 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable" -y
 sudo apt update -y
 
+# sudo apt update -y
+# sudo apt install python3-pip -y
+
 
 
 
