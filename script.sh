ad32b787664b45e9b795e5330008b912c82d9db5
Tarabanov Serhii <tsv198250@gmail.com>
2022-03-18 02:21:09 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-sudo apt update -y
-sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable" -y
-sudo apt update -y
-sudo apt install docker-ce -y
+echo Helloo World
+#sudo apt update -y
+#sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+#sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable" -y
+#sudo apt update -y
+#sudo apt install docker-ce -y
 
 
 # sudo apt update -y
